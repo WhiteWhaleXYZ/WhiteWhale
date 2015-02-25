299312bb9b42f20f67f1dd0ca4a12bb0536dfda0
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-02-25 16:19:47 -0500

    Users created no passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220173903) do
+ActiveRecord::Schema.define(version: 20150225210751) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,14 @@ ActiveRecord::Schema.define(version: 20150220173903) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
