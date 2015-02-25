21c891bc701483db53c2fb6a9d598975d294705c
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-02-25 14:57:15 -0500

    Ran generate users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   resources :articles
   root :to => "articles#index"
+  get 'signup'  => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
