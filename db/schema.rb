f15654e2aa1fd573f070641990c1006b8af872ce
Andrew Joseph Baumann <baumann.andrew14@gmail.com>
2015-02-26 19:53:40 +0000

    just comment reminder

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+=begin
+Make sure we add in tagging 
+=end
+
 ActiveRecord::Schema.define(version: 20150225212908) do
 
   create_table "articles", force: :cascade do |t|
