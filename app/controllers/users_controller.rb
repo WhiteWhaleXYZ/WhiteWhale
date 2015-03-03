f64716190271c5f4858c1f7210de0866722e4bf8
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-03-02 22:47:23 -0500

    logout now functioning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to White Whale!"
       redirect_to @user
       # Handle a successful save.
