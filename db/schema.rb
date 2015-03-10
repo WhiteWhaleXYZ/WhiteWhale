fa82f45ec6c4ac194afea3df5b03131350832bab
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-03-10 17:36:01 -0400

    added remember me to login modal

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-=begin
-Make sure we add in tagging 
-=end
-
-ActiveRecord::Schema.define(version: 20150225212908) do
+ActiveRecord::Schema.define(version: 20150310184958) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -30,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150225212908) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
