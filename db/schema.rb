f68a69d1248a5ce700834a8671472742acbf30d7
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-03-15 18:35:42 -0400

    removing articles leftovers

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315214402) do
-
-
+ActiveRecord::Schema.define(version: 20150315195634) do
 
   create_table "pods", force: :cascade do |t|
     t.text     "description"
@@ -31,14 +29,10 @@ ActiveRecord::Schema.define(version: 20150315214402) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
