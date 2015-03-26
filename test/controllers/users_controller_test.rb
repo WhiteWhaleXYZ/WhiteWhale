f4ab4a6054d13de30f86e543b8cbb54e073fde38
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-03-25 22:11:33 -0400

    finished 9.3

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UsersControllerTest < ActionController::TestCase
     @other_user = users(:archer)
   end
 
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get :new
     assert_response :success
