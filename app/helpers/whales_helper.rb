82ba5bfa56adaa16a268a52b72f9befa0c13646e
Yarley Geffrard <yarley@ynodesigns.com>
2015-03-28 14:22:05 -0400

    Fixed Responsive to login and signup, implemented whales controllers views and routes not completed yet

---
+++ app/helpers/whales_helper.rb
@@ -0,0 +1,2 @@
+module WhalesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
 
   resources :users do 
     resources :pods,  only: [:new, :show, :create, :destroy, :edit, :update]
+    resources :whales,  only: [:new, :show, :create, :destroy, :edit, :update]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
