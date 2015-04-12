abe01009df7fba73e2fd7f5b7e01415da2119c46
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-04-12 19:07:14 -0400

    forking works with pictures and everything

--- app/models/pod.rb
+++ app/models/pod.rb
@@ -7,8 +7,22 @@ class Pod < ActiveRecord::Base
 
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "150x150>" }, :default_url => "DefaultPod.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
-    
+
   amoeba do
     enable
+
+    puts "I was here"
+    nullify :photo_file_name
+    nullify :photo_content_type
+    nullify :photo_file_size
+    nullify :photo_updated_at
+
+    customize(lambda { |orig_obj,copy_of_obj|
+        puts "and here"
+        copy_of_obj.photo = orig_obj.photo
+    })
+
+
+
   end
 end
