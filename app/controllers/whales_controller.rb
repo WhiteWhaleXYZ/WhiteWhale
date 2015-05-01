f2b462802ecef27657c5c8521b29e58b6cb9a6e3
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-04-30 22:47:14 -0400

    user can not edit or update other users whales or pods

--- app/controllers/whales_controller.rb
+++ app/controllers/whales_controller.rb
@@ -1,6 +1,6 @@
 class WhalesController < ApplicationController
   before_action :logged_in_user, only: [:new,:show, :create, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :correct_user,   only: [:new, :create, :destroy]
   #def index
   #end
  
